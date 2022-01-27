f822fbae444aecf10cfb175f7147b67d521a982d
Sreedivya-K <sreedivya0016@msitprogram.net>
2022-01-27 16:39:00 +0530

    Make a Codon Dictionary

--- hw6_protein.py
+++ hw6_protein.py
@@ -60,7 +60,15 @@ Returns: dict mapping strs to strs
 '''
 def makeCodonDictionary(filename):
     import json
-    return
+    file=open(filename,"r")
+    text=json.load(file)
+    codonToAminoAcidDict={}
+    for aminoAcid,codonList in text.items():
+        for codon in codonList:
+            codon=codon.replace('T','U')
+            codonToAminoAcidDict[codon]=aminoAcid
+    return codonToAminoAcidDict
+    
 
 
 '''
